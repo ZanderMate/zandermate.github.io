99a37aa967453e038fe3c36663d89521f403762e
Alex Griep <alexgriep@hotmail.com>
2020-01-27 18:09:20 -0600

    cleaning up da burger app

--- da-burger/db/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE burger (
-    id INT NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(255),
-    devoured BOOLEAN,
-    createAt TIMESTAMP NOT NULL,
-    updatedAt TIMESTAMP NOT NULL,
-    PRIMARY KEY(id)
-);
--- da-burger/routes/html-routes.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(app) {
-
-    app.get("/index", function(req, res) {
-        res.render("index", );
-    });
-}
